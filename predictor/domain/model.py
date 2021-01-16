bf68a45836fd389a4397a750a0f8ee94ac3ef270
Joshua-Luke Bevan <joshua-luke@hotmail.co.uk>
2021-01-16 13:15:50 +0000

    Added compiled flag for model

--- predictor/domain/model.py
+++ predictor/domain/model.py
@@ -1,4 +1,9 @@
 class Model:
 
     def __init__(self, processed_images):
+        self.compiled = False
         self.processed_images = processed_images
+
+    def compile(self):
+        self.compiled = True
+        return self.compiled
--- tests/domain/test_model.py
+++ tests/domain/test_model.py
@@ -1,12 +1,16 @@
 from predictor.domain.model import Model
+from unittest.mock import MagicMock
 
 
 def test_model_init():
-    processed_images = ["./image.png", "./image.jpeg"]
+    processed_images = ["./image.png", "./image.jpg", "./image.jpeg"]
     model = Model(processed_images)
     assert isinstance(model, Model)
     assert model.processed_images == processed_images
 
 
-# def test_model_can_train():
-#     model = Model()
+def test_model_can_be_compiled():
+    processed_images = ["./image.png", "./image.jpg", "./image.jpeg"]
+    model = Model(processed_images)
+    model.compile()
+    assert model.compiled == True
