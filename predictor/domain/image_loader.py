612d4b2a847426aab09456f2482441459fa15a18
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2021-01-22 16:28:52 +0000

    Added image loader tests

--- predictor/domain/image_loader.py
+++ predictor/domain/image_loader.py
@@ -1,4 +1,11 @@
+from pathlib import Path
+
+
 class ImageLoader():
 
-    def __init__(self):
-        pass
+    def __init__(self, url, get_file_method):
+        self.url = url
+        self.get_file_method = get_file_method
+
+    def load(self, name):
+        return Path(self.get_file_method(name, origin=self.url, untar=True))
--- tests/domain/test_image_loader.py
+++ tests/domain/test_image_loader.py
@@ -1,6 +1,21 @@
 from predictor.domain.image_loader import ImageLoader
+from tensorflow.keras.utils import get_file
+import pathlib
+
+from unittest.mock import MagicMock, patch
 
 
 def test_image_loader_init():
-    image_loader = ImageLoader()
+    url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+    image_loader = ImageLoader(url, get_file)
     assert isinstance(image_loader, ImageLoader)
+
+
+# TODO: get this test working properly
+@patch('six.moves.urllib.request')
+def test_can_load_images_from_a_url(mock_urlopen):
+    url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+    mocked_images_path = "path_to_directory_of_images"
+    image_loader = ImageLoader(url, get_file)
+    directory_of_images = image_loader.load('pets')
+    assert directory_of_images == mocked_images_path
